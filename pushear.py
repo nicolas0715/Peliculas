996961655b3cb9e40a1aeffaa7722ca55aa4ea01
Otero <oteronicolas3@gmail.com>
2023-05-31 23:01:26 -0300

    3

--- pushear.py
+++ pushear.py
@@ -4,12 +4,12 @@ from git import Repo
 
 def pushear_html():
     # Ruta local al repositorio
-    repo_path = 'C:/Users/otero/Desktop/Pokio/Programacion/Proyectos/Proyectos_varios/Web_Scrapping4-'
+    #repo_path = 'C:/Users/otero/Desktop/Pokio/Programacion/Proyectos/Proyectos_varios/Web_Scrapping4-'
     # Pero para ejecutarlo con Actions, necesito la ruta local
     ruta_raiz = os.getcwd()
 
     # Inicializar el repositorio
-    repo = Repo(repo_path)
+    repo = Repo(ruta_raiz)
 
     # Agregar los archivos al repositorio (opcional)
     repo.index.add('index.html')
@@ -22,5 +22,5 @@ def pushear_html():
     # Realizar el push al repositorio remoto
     origin = repo.remote('origin')
     origin_url = origin.url.replace('https://', f'https://token:{token}@') 
-    origin.set_url(repo_path)
+    origin.set_url(origin_url)
     origin.push()
